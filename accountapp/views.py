01e973f9d285af486eb83c6ecf5e98559960ed64
chunccc1004 <chuncccc1004@gmail.com>
2024-01-22 19:18:57 +0900

    add: AccountCreatView

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,12 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
 # Create your views here.
-
-
+class AccountCreatView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:')
+    template_name = 'accountapp/create.html'
