3b8c138f6db2d517abced5aa9e45cfdcc46f7a7d
chunccc1004 <chuncccc1004@gmail.com>
2024-01-22 19:18:57 +0900

    add: AccountCreatView

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from accountapp.views import AccountCreatView
+
 app_name = "accountapp"
 
 urlpatterns = [
-
+    path('create/', AccountCreatView.as_view(), name='create'),
 ]
