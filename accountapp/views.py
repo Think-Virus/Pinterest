5154975d3d585564dc04bcabe0eecca946e5a926
chunccc1004 <chuncccc1004@gmail.com>
2024-01-22 21:42:22 +0900

    add: detail view

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 
 # Create your views here.
@@ -10,3 +10,9 @@ class AccountCreatView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/create.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
