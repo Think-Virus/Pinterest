e8be34766eb042b412f236a36c239c708f88b644
chunccc1004 <chuncccc1004@gmail.com>
2024-01-22 21:52:54 +0900

    add: update view
    forms.py에서 UserCreationForm을 상속받아서 원하는 필드 수정 불가는하게 할 수 있음

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
+
+from accountapp.forms import AccountUpdateForm
 
 
 # Create your views here.
@@ -16,3 +18,10 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/update.html'
