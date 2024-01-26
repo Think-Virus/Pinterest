97ff530187dda20a3590affe0fa578d73e58a412
chunccc1004 <chuncccc1004@gmail.com>
2024-01-26 19:08:45 +0900

    add: project app start 및 기본 설정
    setting
    urls 연결
    Model 구현
    View 틀 구현

--- Pinterest/settings.py
+++ Pinterest/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'profileapp',
     'articleapp',
     'commentapp',
+    'profileapp',
 ]
 
 MIDDLEWARE = [
---
+++ projectapp/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Project(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='project', null=True)
+    name = models.CharField(max_length=200, null=True)
+    detail = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='project/', null=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
