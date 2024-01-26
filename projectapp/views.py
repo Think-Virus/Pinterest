6c6c9ad7bef8a2d71c8e95e2055be326498c6b6c
chunccc1004 <chuncccc1004@gmail.com>
2024-01-26 22:41:49 +0900

    add: project create 기능 구현

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import CreateView, DetailView, ListView
 
+from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
 
 # Create your views here.
 class ProjectCreateView(CreateView):
     model = Project
+    form_class = ProjectCreationForm
+    template_name = 'projectapp/create.html'
+
+    def form_valid(self, form):
+        tmp_project = form.save(commit=False)
+        tmp_project.owner = self.request.user
+        tmp_project.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
 
 
 class ProjectDetailView(DetailView):
