05ce9679f4ab54bdc49bf676081591c05989f147
chunccc1004 <chuncccc1004@gmail.com>
2024-01-26 18:52:07 +0900

    refactor: 필요없는 주석 제거

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -7,6 +7,4 @@ app_name = "commentapp"
 urlpatterns = [
     path('create/', CommentCreateView.as_view(), name='create'),
     path('delete/<int:pk>', CommentDeleteView.as_view(), name='delete'),
-    # path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
-    # path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
 ]
