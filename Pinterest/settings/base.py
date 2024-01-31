d2c0c4291a2137522f4af5cf5fa4b96b95941966
chunccc1004 <chuncccc1004@gmail.com>
2024-01-31 14:46:21 +0900

    add: local과 배포 setting 구분

--- Pinterest/settings/base.py
+++ Pinterest/settings/base.py
@@ -9,33 +9,13 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
-
-import environ
+import os.path
 from pathlib import Path
 
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-environ.Env.read_env(
-    env_file=os.path.join(BASE_DIR, '.env')
-)
-
-# SECURITY WARNING: keep the secret key used in production secret!
-env('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Application definition
 
@@ -85,16 +65,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Pinterest.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
