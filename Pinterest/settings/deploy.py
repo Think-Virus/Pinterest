a4d0ac7d7e8c1ae2ad2e7a86bb3bf37a7ed586e1
chunccc1004 <chuncccc1004@gmail.com>
2024-01-31 19:29:26 +0900

    fix: env 대신 docker의 secret 사용하도록 변경

--- Pinterest/settings/deploy.py
+++ Pinterest/settings/deploy.py
@@ -1,27 +1,23 @@
-import environ
-
 from .base import *
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-# reading .env file
-environ.Env.read_env(
-    env_file=os.path.join(BASE_DIR, '.env')
-)
+
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read().strip()
+    file.close()
+    return secret
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
@@ -31,7 +27,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "django",
         "USER": "django",
-        "PASSWORD": env("MYSQL_PASSWORD"),
+        "PASSWORD": read_secret("MYSQL_PASSWORD"),
         "HOST": "mariadb",
         "PORT": "3306",
     }
